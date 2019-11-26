79e8078fc399b15b9b1de572b35fdb29178cec1c
Adam <adam123quiroz@gmail.com>
2019-11-26 00:52:06 -0400

    add class PotSensorData

---
+++ app/src/main/java/com/example/smartpot/entity/PotSensorData.java
@@ -0,0 +1,50 @@
+package com.example.smartpot.entity;
+
+public class PotSensorData {
+    private double ambientTemperature;
+    private double light;
+    private double earthHumidity;
+    private double ambientHumidity;
+
+    public PotSensorData(double ambientTemperature, double light, double earthHumidity, double ambientHumidity) {
+        this.ambientTemperature = ambientTemperature;
+        this.light = light;
+        this.earthHumidity = earthHumidity;
+        this.ambientHumidity = ambientHumidity;
+    }
+
+    public PotSensorData() {
+    }
+
+    public double getAmbientTemperature() {
+        return ambientTemperature;
+    }
+
+    public void setAmbientTemperature(double ambientTemperature) {
+        this.ambientTemperature = ambientTemperature;
+    }
+
+    public double getLight() {
+        return light;
+    }
+
+    public void setLight(double light) {
+        this.light = light;
+    }
+
+    public double getEarthHumidity() {
+        return earthHumidity;
+    }
+
+    public void setEarthHumidity(double earthHumidity) {
+        this.earthHumidity = earthHumidity;
+    }
+
+    public double getAmbientHumidity() {
+        return ambientHumidity;
+    }
+
+    public void setAmbientHumidity(double ambientHumidity) {
+        this.ambientHumidity = ambientHumidity;
+    }
+}
